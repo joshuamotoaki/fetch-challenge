e3ed199ddf0f98b0a829237dc75fbde558d60ee2
Joshua Lau <dachshunduniverse@gmail.com>
2024-10-02 21:38:43 -0400

    Create local storage store

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -1,5 +1,30 @@
-import { writable } from "svelte/store";
+import { get, writable } from "svelte/store";
 
+// Sets to true if the app encounters an error (shows <Failure /> component)
 export const isFailure = writable<boolean>(false);
 
+// List of dog breeds fetched from the Dog API
 export const doglist = writable<string[]>([]);
+
+// Generic persistent store that saves to local storage
+const createLocalStorageStore = <T>(key: string, initialValue: T) => {
+    const store = writable<T>(initialValue);
+
+    return {
+        subscribe: store.subscribe,
+        set: (value: T) => {
+            localStorage.setItem(key, JSON.stringify(value));
+            store.set(value);
+        },
+        update: (updater: (value: T) => T) => {
+            const value = updater(get(store));
+            localStorage.setItem(key, JSON.stringify(value));
+            store.set(value);
+
+            return value;
+        }
+    };
+};
+
+// List of dog breeds saved to local storage
+const savedDoglist = createLocalStorageStore<string[]>("doglist", []);
