89a7ed5d69d5340da0a5225f48612c6960c851e1
Joshua Lau <dachshunduniverse@gmail.com>
2024-10-02 23:12:06 -0400

    Add no image message

--- src/lib/Gallery.svelte
+++ src/lib/Gallery.svelte
@@ -9,16 +9,23 @@
 <main
     id="gallery"
     class="flex-1 shadow-xl border rounded-lg h-full bg-white overflow-y-auto">
-    <div style={dispCss} class="p-2 grid gap-2">
-        <!-- Flexbox per numCols to make gallery have less whitespace -->
-        {#each Array(numCols) as _, i}
-            <div class="flex flex-col gap-2">
-                {#each $currentImages as dogImage, j}
-                    {#if j % numCols === i}
-                        <DogImage {dogImage} dogName="Cat" />
-                    {/if}
-                {/each}
-            </div>
-        {/each}
-    </div>
+    {#if $currentImages.length === 0}
+        <h2 class="text-xl font-semibold text-center mt-8">No images found</h2>
+        <p class="text-lg text-center">
+            Add a breed in the sidebar to see images here.
+        </p>
+    {:else}
+        <div style={dispCss} class="p-2 grid gap-2">
+            <!-- Flexbox per numCols to make gallery have less whitespace -->
+            {#each Array(numCols) as _, i}
+                <div class="flex flex-col gap-2">
+                    {#each $currentImages as dogImage, j}
+                        {#if j % numCols === i}
+                            <DogImage {dogImage} dogName="Cat" />
+                        {/if}
+                    {/each}
+                </div>
+            {/each}
+        </div>
+    {/if}
 </main>
